91bb45447b7e4a81e7efa3c6265be0bcba9f1e98
OfekShochat <o@shoch.at>
2021-04-12 18:15:02 +0300

    im always pushing my token, huh?

--- main.py
+++ main.py
@@ -176,4 +176,4 @@ async def play(ctx, url):
 async def on_message(message):
     await bot.process_commands(message) # Process the message into a command
 
-bot.run('ODIyMzM3NDI4ODE5MDE3NzM5.YFQzaQ.komfMHNWR9jjmtOhj2rMa8almvs') # The bot "password", this is needed to connect to the account.
+bot.run('') # The bot "password", this is needed to connect to the account.
