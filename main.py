d582302f9d12fe07a160ee72b944cf27cef06623
OfekShochat <o@shoch.at>
2021-04-12 20:42:32 +0300

    nani

--- main.py
+++ main.py
@@ -179,4 +179,4 @@ async def play(ctx, url):
 async def on_message(message):
     await bot.process_commands(message) # Process the message into a command
 
-bot.run('ODIyMzM3NDI4ODE5MDE3NzM5.YFQzaQ.hO5cJGBT3BRL49Wz9b7uFp95C7E') # The bot "password", this is needed to connect to the account.
+bot.run('') # The bot "password", this is needed to connect to the account.
